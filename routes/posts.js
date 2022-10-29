cadf589bbf09d6a429bff740e3c4bccbb5429e8d
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-10-29 09:40:58 +0200

    Implement fetching post comments

--- routes/posts.js
+++ routes/posts.js
@@ -9,10 +9,8 @@ router.get('/feed', authToken, (request, response) => {
     try {
         const userId = jwt.decode(token).id_user;
         hub.dbPool.query(`
-            SELECT p.*, row_to_json(u.*) AS user,
-                jsonb_agg(c.*) FILTER (WHERE c.id_comment IS NOT NULL) AS comments
+            SELECT p.*, row_to_json(u.*) AS user
             FROM posts p
-                LEFT JOIN comments c on p.id_post = c.id_post
                 LEFT JOIN users u on p.id_user = u.id_user
             GROUP BY p.id_post, u.id_user`, (error, results) => {
             if (error) return response.status(500).send(error.description);
@@ -22,6 +20,20 @@ router.get('/feed', authToken, (request, response) => {
         console.log(error);
         return response.sendStatus(400);
     }
+})
+
+router.get('/:id/comments', authToken, (request, response) => {
+    const id = parseInt(request.params.id);
+    if (isNaN(id))
+        return response.status(400).send('invalid id');
+    hub.dbPool.query(`
+        SELECT c.*, row_to_json(u.*) AS user
+        FROM comments c
+        LEFT JOIN users u ON c.id_user = u.id_user
+        WHERE c.id_post = $1`, [id], (error, results) => {
+        if (error) return response.status(500).send(error.description);
+        response.status(200).json(results.rows);
+    })
 })
 
 module.exports = router;
