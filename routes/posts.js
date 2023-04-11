c0a8774694abe5c824b4691b7f8ef7bad7cb9697
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-04-11 19:30:54 +0200

    [WIP] Posts Refactor

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const hub = require('hub');
-const {authToken} = require('../utils');
+const {authToken, userIdFromAuthHeader} = require('../utils');
 const uniqueId = require('uniqid');
-const jwt = require("jsonwebtoken");
 const path = require("path");
-const exists = require('property-exists');
 
 router.get('/feed', authToken, (request, response) => {
     // user limiting disabled for testing purposes
@@ -105,47 +103,36 @@ router.delete('/:id/comment/:comment', authToken, (request, response) => {
 router.post('/:id/like', authToken, (request, response) => {
     const id = parseInt(request.params.id);
     if (isNaN(id))
-        return response.status(400).send('invalid id');
-    const authHeader = request.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    try {
-        const userId = jwt.decode(token).id_user;
-        hub.dbPool.query(`
-            INSERT INTO reactions (id_post, id_user)
-            VALUES ($1, $2)`, [id, userId], (error) => {
-            if (exists(error, 'code'))
-                if (error.code === '23505') // already liked
-                    return response.status(400).send('only one such reaction per post per user allowed')
-            if (error)
-                return response.status(500).send(error.description);
-            response.sendStatus(200);
+        return response.status(400).send('Invalid ID');
+    const userId = userIdFromAuthHeader(request);
+    hub.dbPool
+        .query(`INSERT INTO reactions (reaction, id_post, id_user)
+                VALUES ('like', $1, $2)`, [id, userId])
+        .then(() => response.sendStatus(200))
+        .catch(error => {
+            if (error.code === '23505') // already liked
+                return response.status(400).send('Only one such reaction per post per user is allowed')
+            console.log(error.stack)
+            response.sendStatus(500);
         });
-    } catch (error) {
-        return response.status(500).send(error);
-    }
-})
+});
 
-router.post('/:id/unlike', authToken, (request, response) => {
+router.delete('/:id/like', authToken, (request, response) => {
     const id = parseInt(request.params.id);
     if (isNaN(id))
-        return response.status(400).send('invalid id');
-    const authHeader = request.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    try {
-        const userId = jwt.decode(token).id_user;
-        hub.dbPool.query(`DELETE
-                          FROM reactions
-                          WHERE (id_post = $1)
-                            AND (id_user = $2)
-                            AND (reaction = $3)`, [id, userId, 'like'], (error) => {
-            if (error)
-                return response.status(500).send(error.description);
-            response.status(200).send('unliked');
+        return response.status(400).send('Invalid ID');
+    const userId = userIdFromAuthHeader(request);
+    hub.dbPool
+        .query(`DELETE FROM reactions
+                WHERE (id_post = $1)
+                  AND (id_user = $2)
+                  AND (reaction = 'like')`, [id, userId])
+        .then(() => response.sendStatus(200))
+        .catch(error => {
+            console.log(error.stack)
+            response.sendStatus(500);
         });
-    } catch (error) {
-        return response.status(500).send(error);
-    }
-})
+});
 
 router.post('/:type', authToken, (request, response) => {
     try {
@@ -188,28 +175,40 @@ router.post('/:type', authToken, (request, response) => {
     }
 })
 
+/*router.post('/video', authToken, (request, response) => {
+    if (!request.files || !request.files.video)
+        return response.status(400).send('File missing');
+    const {title, description} = request.body;
+    if (!title)
+        return response.status(400).send('Missing required parameters');
+    const identifier = uniqueId();
+    const userId = userIdFromAuthHeader(request);
+    const video = request.files.video;
+
+    const fileName = identifier + '_' + video.name;
+    const filePath = path.join(__dirname, '..', 'public', 'posts', 'videos', fileName);
+    video.mv(filePath)
+        .then(() => hub.dbPool.query(
+            `INSERT INTO posts (identifier, title, description, content_uri, id_user)
+             VALUES ($1, $2, $3, $4, $5)`,
+             [identifier, title, description, fileName, userId]))
+        .then(results => response.json(results.rows[0]))
+        .catch(error => {
+            console.log(error);
+            response.sendStatus(500);
+        });
+});*/
+
 router.delete('/:id', authToken, (request, response) => {
     const id = parseInt(request.params.id);
     if (isNaN(id))
-        return response.status(400).send('invalid id');
+        return response.status(400).send('Invalid ID');
     hub.dbPool
         .query('DELETE FROM reactions WHERE id_post = $1', [id])
-        .then(() => {
-            hub.dbPool.query('DELETE FROM comments WHERE id_post = $1', [id])
-                .then(() => {
-                    hub.dbPool.query('DELETE FROM posts WHERE id_post = $1', [id])
-                        .then(() => response.sendStatus(200))
-                        .catch((error) => {
-                            console.log(error.stack);
-                            return response.sendStatus(500);
-                        });
-                })
-                .catch((error) => {
-                    console.log(error.stack);
-                    return response.sendStatus(500);
-                });
-        })
-        .catch((error) => {
+        .then(() => hub.dbPool.query('DELETE FROM comments WHERE id_post = $1', [id]))
+        .then(() => hub.dbPool.query('DELETE FROM posts WHERE id_post = $1', [id]))
+        .then(() => response.sendStatus(200))
+        .catch(error => {
             console.log(error.stack);
             return response.sendStatus(500);
         });
